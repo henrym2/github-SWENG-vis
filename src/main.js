ff970bac707efab95c26ca03352f9b97b4a4eb62
henrym2 <33910429+henrym2@users.noreply.github.com>
2019-11-04 15:49:14 +0000

    Added oktokit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Octokit from "@octokit/rest"
+require('dotenv').config()
 
+const octokit = Octokit({
+  auth: process.env.GITHUB_TOKEN,
+  userAgent: 'SWENG github vis',
+})
+
+// Object.definePrototype(Vue.prototype, '$octokit', { value: octokit });
+Vue.prototype.$octokit = octokit
 Vue.config.productionTip = false
 
 new Vue({
