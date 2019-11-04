e5fbfc34fe3273a4befb983a43db2d4e58414b85
henrym2 <33910429+henrym2@users.noreply.github.com>
2019-11-04 15:48:57 +0000

    Added user card

---
+++ src/components/UserCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="d-flex card shadow" style="width: 20rem;">
+      <img v-bind:src="userData.avatar_url" alt="https://upload.wikimedia.org/wikipedia/commons/7/72/Default-welcomer.png" class="card-img-top img-fluid">
+      <div class="card-body">
+        <h5 class="card-title">{{userData.name}}</h5>
+        <p class="card-text mb-0">Followers: {{userData.followers}}</p>
+        <p v-if="userData.company" class="card-text mb-0">Company: {{userData.company}}</p>
+        <p class="card-text mb-0">Bio: {{userData.bio}}</p>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+      return{
+      }
+  },
+  props:{
+      userData: Object
+  },
+  methods: {
+  
+  }
+}
+</script>
+
+<style>
+
+</style>
