5ab1d9711f871f301707ccef25d3f1d7aec0d21c
henrym2 <33910429+henrym2@users.noreply.github.com>
2019-11-12 14:49:31 +0000

    fixed tests

--- __tests__/app.spec.js
+++ __tests__/app.spec.js
@@ -1,10 +1,20 @@
-import { mount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 import App from './../src/App.vue'
 import { toBindingIdentifierName } from '@babel/types'
+import {
+    createLocalVue
+} from '@vue/test-utils'
+import BootstrapVue from 'bootstrap-vue'
+
+// create an extended `Vue` constructor
+const localVue = createLocalVue()
+
+// install plugins as normal
+localVue.use(BootstrapVue)
 
 describe('App',() => {
     test("Is rendering", () => {
-        const wrapper = mount(App)
+        const wrapper = shallowMount(App, {localVue})
         expect(wrapper.vm.currentUser).toBe("")
         const navButton = wrapper.find('#nav-button-1')
         navButton.exists()
