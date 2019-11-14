ad4174b92b2fc391b69fba8a508783c4dbb1fd0d
henrym2 <33910429+henrym2@users.noreply.github.com>
2019-11-14 00:33:05 +0000

    Added fix for long repo names

--- src/components/StarredRepos.vue
+++ src/components/StarredRepos.vue
@@ -4,14 +4,17 @@
       <h5 class="card-title mt-0">
           Starred Repos
       </h5>
-      <p class="mb-0" v-for="star in starred" v-bind:key=star.id>{{star.name}}
-          <a class="mr-auto" target="_blank" v-bind:href="star.url">
+      <p class="mb-0" v-for="star in starred" v-bind:key=star.id>{{buildName(star)}}
+          <a class="mr-auto" target="_blank" v-bind:href="star.url" v-bind:id="'star'+star.id" >
               <i class="fa fa-link"></i>
         </a>
+        
       </p>
 
       </div>
-
+    <b-tooltip v-for="star in bigStars" v-bind:key="star.id" v-bind:target="'star'+star.id" triggers="hover focus" delay="{ show: 100, hide: 500}">
+            {{star.name}}
+    </b-tooltip>
   </div>
 </template>
 
@@ -24,6 +27,14 @@ export default {
     },
     created: function() {
         this.getstarred()
+    },
+    computed: {
+        bigStars: function(){
+            if(this.starred != null){
+            return this.starred.filter(e => e.tooLong)
+            }
+            return []
+        }
     },
     props: {
         userName: null
@@ -45,12 +56,21 @@ export default {
                     obj['name'] = e.full_name
                     obj['id'] = e.id
                     obj['url'] = e.html_url
+                    obj['tooLong'] = false
                     return obj
                 })
             }).catch(err => {
                 // eslint-disable-next-line no-console
                 console.log(err)
             })
+        },
+        buildName: function(star) {
+            if(star.name.length > 24) {
+                star.tooLong = true
+                return star.name.slice(0,24)+"..."
+            }
+            else return star.name
+            
         }
     }
 }
