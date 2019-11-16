1dde0bcf118036493db64b595ecfda5d9d93d064
henrym2 <33910429+henrym2@users.noreply.github.com>
2019-11-16 18:07:43 +0000

    Added modal to list all starred repos

--- src/components/StarredRepos.vue
+++ src/components/StarredRepos.vue
@@ -1,20 +1,34 @@
 <template>
   <div class="d-flex card shadow">
       <div class="card-body">
-      <h5 class="card-title mt-0">
-          Starred Repos
-      </h5>
-      <p class="mb-0" v-for="star in starred" v-bind:key=star.id>{{buildName(star)}}
+        <div class="d-flex flex-row">
+            <h5 class="card-title mt-0">
+                Starred Repos
+            </h5>
+           <a class="ml-auto click-able" href="#"><i class="fa fa-arrows-alt" v-on:click="showMore()"></i></a>
+        </div>
+      <p class="mb-0" v-for="star in starred.slice(0,4)" v-bind:key=star.id>{{buildName(star)}}
           <a class="mr-auto" target="_blank" v-bind:href="star.url" v-bind:id="'star'+star.id" >
               <i class="fa fa-link"></i>
-        </a>
-        
+              </a>  
       </p>
 
       </div>
     <b-tooltip v-for="star in bigStars" v-bind:key="star.id" v-bind:target="'star'+star.id" triggers="hover focus" delay="{ show: 100, hide: 500}">
             {{star.name}}
     </b-tooltip>
+    <b-modal id="all-stars" hide-footer scrollable hide-backdrop content-class="shadow-lg" cancel-disabled>
+        <template v-slot:modal-title>
+            Starred repos
+        </template>
+        <div class="d-flex flex-column">
+            <p class="mb-0" v-for="star in starred" v-bind:key=star.id>{{star.name}}
+                <a class="mr-auto" target="_blank" v-bind:href="star.url" v-bind:id="'star'+star.id" >
+                    <i class="fa fa-link"></i>
+                </a>  
+            </p>
+        </div>
+    </b-modal>
   </div>
 </template>
 
@@ -49,7 +63,7 @@ export default {
             this.$octokit.activity.listReposStarredByUser({
                 username: this.userName,
                 sort:     "updated",
-                per_page: 4
+                per_page: 100
             }).then(res => {
                 this.starred = res.data.map(e => {
                     let obj = {}
@@ -71,11 +85,17 @@ export default {
             }
             else return star.name
             
+        },
+        showMore: function(){
+            this.$bvModal.show("all-stars")
         }
     }
 }
 </script>
 
 <style>
-
+    #click-able{
+        cursor: pointer;
+        color: black;
+    }
 </style>
